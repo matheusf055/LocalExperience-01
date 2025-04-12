a234e2798842d08093b4f1bf5a3a52856d37541c
Matheus Fernandes <m.cesar.nunes2014@gmail.com>
2025-04-12 12:59:27 -0300

    Adiciona a interface do appservice de TripsInterest

---
+++ LocalExperience.AppServices/Interfaces/TripsInterestProfiles/DTOs/TripsInterestProfileCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using LocalExperience.Domain.Trips;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalExperience.AppServices.Interfaces.TripsInterestProfiles.DTOs
+{
+    public class TripsInterestProfileCreateDto
+    {
+        public Guid TripId { get; set; }
+        public InterestLevel CultureInterest { get; set; }
+        public InterestLevel NatureInterest { get; set; }
+        public InterestLevel GastronomyInterest { get; set; }
+        public InterestLevel NightlifeInterest { get; set; }
+        public InterestLevel ShoppingInterest { get; set; }
+    }
+}
