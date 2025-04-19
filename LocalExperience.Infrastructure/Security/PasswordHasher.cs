f7357001641357983b95a4eaf835a00b418b2f0c
Matheus Fernandes <m.cesar.nunes2014@gmail.com>
2025-04-19 13:18:42 -0300

    Cria as controllers

--- LocalExperience.Infrastructure/Security/PasswordHasher.cs
+++ LocalExperience.Infrastructure/Security/PasswordHasher.cs
@@ -13,9 +13,9 @@ namespace LocalExperience.Infrastructure.Security
             return BCrypt.Net.BCrypt.HashPassword(password);
         }
 
-        public static bool VerifyPassword(string hashedPassword, string providedPassword)
+        public static bool VerifyPassword(string providedPassword, string hashedPassword)
         {
-            return BCrypt.Net.BCrypt.Verify(hashedPassword, providedPassword);
+            return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
         }
     }
 }
